ed973b3da26df9d6e7f91a98efb87963c904eac8
Aadarsh bikram Shah <np03cs4a220503@heraldcollege.edu.np>
2024-12-17 14:28:23 +0545

    Backend work for login and forgot-password

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    // public function store(LoginRequest $request): Response
+    // {
+    //     $request->authenticate();
+
+    //     $request->session()->regenerate();
+
+    //     return response()->noContent();
+    // }
+    public function store(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (auth()->attempt($credentials)) {
+            $token = auth()->user()->createToken('authToken')->plainTextToken;
+
+            return response()->json(['token' => $token], 200);
+        }
+
+        return response()->json(['message' => 'Unauthorized'], 401);
+        // return $this->error("Credentials do not match", 401);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): Response
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return response()->noContent();
+    }
+}
